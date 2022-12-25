f52710f3d090dca7eb925f28623e488d4386a082
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-26 07:41:24 +0800

    base: Bring in Weather service API
    This commit does not require any WeatherJaw commits in FWB or adding back weather parts.
    Am Keeping Google's Weather Slice for lockscreen weather.
    
    Special Thanks to @omnirom for the OmniJawsClient and services package
    @AICP for their statusbar weather base code adapted for QS Panel and A11
    Ref: https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/49630
    
    @StarkDroid: Forward and align it with A12 QS Header
    
    Change-Id: Iafa109a166a6c4106b15c4bff4dfd322718bed84
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Trishiraj <gamerprince.exp@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -54,6 +54,7 @@ import com.android.systemui.util.LargeScreenUtils;
 import com.android.systemui.tuner.TunerService;
 
 import lineageos.providers.LineageSettings;
+import android.provider.Settings;
 
 import java.util.List;
 
@@ -77,6 +78,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
             "system:" + Settings.System.QS_SHOW_BATTERY_PERCENT;
     private static final String QS_SHOW_BATTERY_ESTIMATE =
             "system:" + Settings.System.QS_SHOW_BATTERY_ESTIMATE;
+    private static final String QS_WEATHER_POSITION =
+            "system:" + Settings.System.QS_WEATHER_POSITION;
 
     private boolean mExpanded;
     private boolean mQsDisabled;
@@ -99,6 +102,9 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private View mStatusIconsView;
     private View mContainer;
 
+    private View mQsWeatherView;
+    private View mQsWeatherHeaderView; 
+
     private View mQSCarriers;
     private ViewGroup mClockContainer;
     private Clock mClockView;
@@ -113,6 +119,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private BatteryMeterView mBatteryIcon;
     private StatusIconContainer mIconContainer;
     private View mPrivacyChip;
+    
+    private int mQQSWeather;
 
     @Nullable
     private TintedIconManager mTintedIconManager;
@@ -172,6 +180,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mPrivacyChip = findViewById(R.id.privacy_chip);
         mDateView = findViewById(R.id.date);
         mClockDateView = findViewById(R.id.date_clock);
+        mQsWeatherView = findViewById(R.id.qs_weather_view);
+        mQsWeatherHeaderView = findViewById(R.id.weather_view_header);
         mClockIconsSeparator = findViewById(R.id.separator);
         mRightLayout = findViewById(R.id.rightLayout);
         mDateContainer = findViewById(R.id.date_container);
@@ -206,7 +216,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                 QS_BATTERY_STYLE,
                 QS_BATTERY_LOCATION,
                 QS_SHOW_BATTERY_PERCENT,
-                QS_SHOW_BATTERY_ESTIMATE);
+                QS_SHOW_BATTERY_ESTIMATE,
+                QS_WEATHER_POSITION);
     }
 
     void onAttach(TintedIconManager iconManager,
@@ -383,6 +394,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                 // These views appear on expanding down
                 .addFloat(mDateView, "alpha", 0, 0, 1)
                 .addFloat(mClockDateView, "alpha", 1, 0, 0)
+                .addFloat(mQsWeatherHeaderView, "alpha", 0, 0, 1)
+                .addFloat(mQsWeatherView, "alpha", 1, 0, 0)
                 .addFloat(mQSCarriers, "alpha", 0, 1)
                 // Use statusbar paddings when collapsed,
                 // align with QS when expanded, and animate translation
@@ -653,6 +666,19 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         return mBatteryIcon;
     }
 
+    private void updateQSWeatherPosition() {
+        if (mQQSWeather == 0) {
+            mQsWeatherHeaderView.setVisibility(View.GONE);
+            mQsWeatherView.setVisibility(View.VISIBLE);
+        } else if (mQQSWeather == 1) {
+            mQsWeatherHeaderView.setVisibility(View.VISIBLE);
+            mQsWeatherView.setVisibility(View.GONE);
+        } else {
+            mQsWeatherHeaderView.setVisibility(View.VISIBLE);
+            mQsWeatherView.setVisibility(View.VISIBLE);
+        }
+    }
+
     @Override
     public void onTuningChanged(String key, String newValue) {
         switch (key) {
@@ -701,6 +727,11 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                 mBatteryIcon.setBatteryEstimate(
                         TunerService.parseInteger(newValue, 0));
                 setChipVisibility(mPrivacyChip.getVisibility() == View.VISIBLE);
+                break;
+            case QS_WEATHER_POSITION:
+                mQQSWeather =
+                       TunerService.parseInteger(newValue, 2);
+                updateQSWeatherPosition();
                 break;
             default:
                 break;
