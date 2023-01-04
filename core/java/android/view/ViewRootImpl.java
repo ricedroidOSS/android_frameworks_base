acb1afd72001698c5bde8d7324bb24652fe6dfd1
Dmitry Vykochko <vykochko@google.com>
2023-01-04 10:07:59 +0000

    Fix a11y focus rendering on non-dirty root view.
    ViewRootImpl can receive TYPE_VIEW_ACCESSIBILITY_FOCUSED, which
    means it should draw the a11y focus. But without any other
    triggers (e.g. animation or scrolling) it is not being rendered.
    This event invalidates the renderer, which makes the focus rectangle
    dirty, but nothing make the draw() method of the view to be called.
    Fixed by adding the whole view invalidation.
    
    Fix: 264356970
    Test: manual
    Change-Id: I4073974551daf622a0c467bfb27df5f5a14ba82c

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -5167,9 +5167,8 @@ public final class ViewRootImpl implements ViewParent,
         mAccessibilityFocusedHost = view;
         mAccessibilityFocusedVirtualView = node;
 
-        if (mAttachInfo.mThreadedRenderer != null) {
-            mAttachInfo.mThreadedRenderer.invalidateRoot();
-        }
+        requestInvalidateRootRenderNode();
+        invalidate();
     }
 
     boolean hasPointerCapture() {
@@ -8210,7 +8209,7 @@ public final class ViewRootImpl implements ViewParent,
         }
 
         mLastTransformHint = transformHint;
-      
+
         mSurfaceControl.setTransformHint(transformHint);
 
         if (mAttachInfo.mContentCaptureManager != null) {
